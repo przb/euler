9f03c7201958145d412c9ee3ff3bff7c5dde2378
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2021-07-05 18:45:28 -0500

    restarted project 11

--- p11_product_grid.py
+++ p11_product_grid.py
@@ -1,31 +1,64 @@
+# In the 20×20 grid below, four numbers along a diagonal 
+# line have been marked in red.
+# 
+# (text file)
+# 
+# The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+# 
+# What is the greatest product of four adjacent 
+# numbers in the same direction (up, down, left, 
+# right, or diagonally) in the 20×20 grid?
+# 
+
+
 # Open and read the file
 file = open("p11_number_grid.txt", "r")
-line = file.readlines()
+rawFileData = file.readlines()
 file.close()
 
-# initialize empty variables
+# make some temp lists for data
+fileData = []
+tempList = []
+
+# proccess the data to a two dimensional list and convert to ints
+for line in rawFileData:
+    line = line.strip().split(" ")
+    for digit in line:
+        #print(digit)
+        tempList.append(int(digit))
+    line = tempList
+    fileData.append(tempList)
+    tempList = []
+
+
+def horz_mult(row, ADJACENT_NUMS):
+    i = 0
+    maxProduct = 1
+    while i <= len(row) - ADJACENT_NUMS:
+        tempProduct = 1
+        multipliers = row[i:i + ADJACENT_NUMS]
+        for multiplier in multipliers:
+            tempProduct *= multiplier
+        if maxProduct < tempProduct:
+            maxProduct = tempProduct
+        i += 1
+    return maxProduct
+def vert_mult(twoDList, ADJACENT_NUMS):
+    
+    i = 0
+    col = 0
+    maxProduct = 1
+    while i < len(twoDList) - ADJACENT_NUMS:
+
+        multipliers = twoDList[i][col]
+        print(multipliers)
+        i += 1
+
+
 i = 0
 j = 0
-abs = 1 # absolute index, total number of index
-data = []
 
-
-# Parse the text file to a list, each row is a list in the list
-for digits in line:
-    row = digits.strip().split(" ")
-    data.append(row)
-# print(data)
-
-
-
-while i < len(data):
-    while j < len(data[i]):
-        print(f"{abs}:{data[i][j]}")
-        j += 1
-        abs += 1
-    i += 1
-    j = 0
-     
-# for a in data:
-#     for b in a:
-#         print(b)
+ADJACENT_NUMS = 4
+# for fileRow in fileData:
+#    print(horz_mult(fileRow, ADJACENT_NUMS))
+vert_mult(fileData, ADJACENT_NUMS)
