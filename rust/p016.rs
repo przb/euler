a1c068c79709c091b88cc323ad092cffcc2812db
Ben P <przybyszewski.benjamin@gmail.com>
2021-11-21 22:44:20 -0600

    Worked on print formatting

--- rust/p016.rs
+++ rust/p016.rs
@@ -7,16 +7,20 @@ What is the sum of the digits of the number 2^1000?
 fn main() {
     let base: u128 = 2;
     let mut total: i32 = 0;
-    for i in 1..=64 {
+    for i in 1..=128 {
         let solution: u128 = base.pow(i);
         let sol = solution.to_string();
         let last_total = total;
         total = 0;
         for digit in sol.chars() {
             total += digit.to_digit(10).unwrap() as i32;
+
         }
         let difference: i32 = total - last_total;
-        //println!("{} + {} = {}", last_total, difference, total);
-        println!("{}", difference);
+        println!("2^{} = {}",i, solution);
+        println!("{:^10}", format!("{:?}", total));
+        println!("{:^10}Test", format!("{:?}", difference));
+        println!("{:width$}", "{}", width = 5, i);
+        //println!("{}", difference);
     }
 }
