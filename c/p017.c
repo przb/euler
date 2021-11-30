fc1babbfe9667d8e8b32023775fa6ffc3e9ebfd2
Ben P <przybyszewski.benjamin@gmail.com>
2021-11-30 12:33:02 -0600

    Counter methods

--- c/p017.c
+++ c/p017.c
@@ -11,7 +11,70 @@ contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
 The use of "and" when writing out numbers is in compliance with British usage.
 */
 
+const int ONE = 3;
+const int TWO = 3;
+const int THREE = 5;
+const int FOUR = 4;
+const int FIVE = 4;
+const int SIX = 3;
+const int SEVEN = 5;
+const int EIGTH = 5;
+const int NINE = 4;
+const int TEN = 3;
+const int ELEVEN = 3;
+const int TWELVE = 3;
+const int THIRTEEN = 3;
+const int FOURTEEN = 3;
+const int FIFTEEN = 3;
+const int SIXTEEN = 3;
+const int SEVENTEEN = 3;
+const int EIGHTEEN= 3;
+const int NINETEEN = 3;
+const int TWENTY = 3;
+const int THIRTY = 3;
+const int FORTY = 3;
+const int FIFTY = 3;
+const int SIXTY = 3;
+const int SEVENTY = 3;
+const int EIGHTY = 3;
+const int NINETY = 3;
+const int HUNDRED = 3;
+const int THOUSAND = 3;
 
+
+int numOnes(int n)
+{
+    switch (n)
+    {
+    case 1:
+        return 3;
+        break;
+    case 2:
+        return 0;
+        break;
+    case 3:
+        return 0;
+        break;
+    case 4:
+        return 0;
+        break;
+    case 5:
+        return 0;
+        break;
+    case 6:
+        return 0;
+        break;
+    case 7:
+        return 0;
+        break;
+    case 8:
+        return 0;
+        break;
+    case 9:
+        return 0;
+        break;
+    }
+}
 
 int main()
 {
