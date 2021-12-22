4ef1df8034a382b20ab4b774eb815c92b14d1d31
Ben P <przybyszewski.benjamin@gmail.com>
2021-12-22 10:14:00 -0600

    Start work on the main path algorithm

--- rust/p018.rs
+++ rust/p018.rs
@@ -17,13 +17,14 @@ be solved by brute force, and requires a clever method! ;o)
 
  */
 use std::fs;
+const MAIN_ARRAY_LENGTH: usize = 15;
 
-fn parse_file_contents(filename: &str) -> Vec<Vec<u8>> {
+fn parse_file_contents(filename: &str) -> Vec<Vec<u32>> {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let mut parsed_contents: Vec<Vec<u8>> = Vec::new();
+    let mut parsed_contents: Vec<Vec<u32>> = Vec::new();
     for line in contents.lines() {
-        let mut vec: Vec<u8> = Vec::new();
+        let mut vec: Vec<u32> = Vec::new();
         for token in line.split_ascii_whitespace() {
             vec.push(token.parse().unwrap());
         }
@@ -32,10 +33,38 @@ fn parse_file_contents(filename: &str) -> Vec<Vec<u8>> {
     return parsed_contents;
 }
 
+fn find_path(contents: &Vec<Vec<u32>>) {
+    let path: [u32; MAIN_ARRAY_LENGTH] = [0; MAIN_ARRAY_LENGTH];
+    let mut current_sum: u32 = contents[0][0].into();
+    for i in 0..contents.len() - 1 {
+        let line = contents[i].clone();
+        let next_line = contents[i + 1].clone();
+        for j in 0..line.len() {
+            let sum_one = current_sum + next_line[j];
+            let sum_two = current_sum + next_line[j + 1];
+            println!("Sum one: {} + {} = {}", line[j], next_line[j], sum_one);
+            println!("Sum two: {} + {} = {}", line[j], next_line[j + 1], sum_two);
+            if sum_one > sum_two {
+                current_sum = sum_one;
+            }else{
+                current_sum = sum_two;
+            }
+        }
+    }
+}
+
+// Current Algorithm
+/**
+ * for each number in the current row
+ *      find the sum of both of the options in the next row
+ * use the biggest sum
+ */
+
 fn main() {
     let filename = "../text-files/p018_triangle_num_sum.txt";
 
     let parsed_contents = parse_file_contents(filename);
 
-    println!("{:?}", parsed_contents);
+    find_path(&parsed_contents);
+    // println!("{:?}", parsed_contents);
 }
